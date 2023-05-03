92aee7676625069c3fe3bc3c11fd8ac922b8aebe
Mbiki-KK <wawerumbiki@gmail.com>
2023-05-03 11:04:23 +0300

    making a function for hamburger icon

--- weightloss/src/components/Navbar.jsx
+++ weightloss/src/components/Navbar.jsx
@@ -2,11 +2,18 @@ import React from 'react'
 import { MenuData } from './MenuData'
 import "./Navbar.css"
 const Navbar = () => {
+    state = {clicked: false};
+    function handleClick () {
+        this.setState({clicked: !this.state.clicked})
+    }
   return (
     <div>
       <nav className='Navbar'>
         <h1 className='logo'><i className='fab fa-react'></i>Nutrition For Weight Loss </h1>
-        <ul>
+        <div className='menu-icons' onClick={this/handleClick}>
+            <i className= {this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>
+        </div>
+        <ul className='nav-menu'>
             {MenuData.map((item, index)=> {
                 return (
                     <li key={index}>
