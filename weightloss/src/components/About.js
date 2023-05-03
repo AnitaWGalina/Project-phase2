43d068d25a200e48b6daabae3d3a2a0d65ff60ae
paul kabuu <kongkabuu9@gmail.com>
2023-05-03 15:22:11 +0300

    added about

---
+++ weightloss/src/components/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <p className="about">
+        Interested in changing your life? Want to see perennial changes? This is
+        the perfect program for you. Our Nutrition app has been specifically
+        curated to cater to your personal needs in an effort to help you reach
+        your target. Want to find out more ? Click on the Home Page to receive
+        your optimum result.
+      </p>
+    </div>
+  );
+}
+
+export default About;
