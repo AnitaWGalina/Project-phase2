19d38ad21cb3a900c069409533c6bcf63f6a5be0
Mbiki-KK <wawerumbiki@gmail.com>
2023-05-03 12:23:19 +0300

    Hero section added

--- weightloss/src/components/Navbar.jsx
+++ weightloss/src/components/Navbar.jsx
@@ -1,17 +1,20 @@
+import { useState } from 'react'
 import React from 'react'
 import { MenuData } from './MenuData'
 import "./Navbar.css"
+
 const Navbar = () => {
-    state = {clicked: false};
-    function handleClick () {
-        this.setState({clicked: !this.state.clicked})
+    const[state,setState]=useState(false)
+    
+    const handleClick = () =>{
+        setState(!state) 
     }
   return (
     <div>
       <nav className='Navbar'>
-        <h1 className='logo'><i className='fab fa-react'></i>Nutrition For Weight Loss </h1>
-        <div className='menu-icons' onClick={this/handleClick}>
-            <i className= {this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>
+        <h1 className='logo'><i className='fab fa-react'></i>Diet Generator </h1>
+        <div className='menu-icons' onClick={handleClick}>
+            <i className= {state? "fas fa-times" : "fas fa-bars"}></i>
         </div>
         <ul className='nav-menu'>
             {MenuData.map((item, index)=> {
