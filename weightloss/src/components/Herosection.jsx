da42ebb5c5f33477b21482978b564b094f33445b
Mbiki-KK <wawerumbiki@gmail.com>
2023-05-03 12:30:35 +0300

    Herosection css added

--- weightloss/src/components/Herosection.jsx
+++ weightloss/src/components/Herosection.jsx
@@ -1,1 +1,12 @@
- 
+import React from "react";
+import ".Herosection.css";
+function Hero() {
+  return (
+    <div className="hero">
+      <div>
+        <a>Start</a>
+      </div>
+    </div>
+  );
+}
+export default Hero;
