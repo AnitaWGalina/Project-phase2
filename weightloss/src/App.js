c18bb084b7d8186a263fc189db5ad4ebfb53c38b
anitagalina <anitagalina21@gmail.com>
2023-05-04 16:09:59 +0300

    Updated weighloss program

--- weightloss/src/App.js
+++ weightloss/src/App.js
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-import Homepage from "./components/Homepage";
-import Navbar from "./components/Navbar";
-
-function App() {
-  return (
-    <main className="App">
-      <Navbar />
-      <Homepage />
-    </main>
-=======
-import './App.css';
 import React from "react";
 import {Routes, Route} from 'react-router-dom'
 import Footer from "./components/Footer";
@@ -17,6 +5,11 @@ import Navbar from './components/Navbar';
 import Hero from './components/Herosection';
 import About from './components/About';
 import FAQs from './components/FAQs';
+import Homepage from "./components/Homepage";
+import Navbar from "./components/Navbar";
+
+
+
 function App() {
   return (
     <div className="App">
@@ -30,7 +23,6 @@ function App() {
       </Routes>
      <Footer/>
     </div>
->>>>>>> f22999ab220e3cb46727b5ecb1514ea0d3cd99d9
   );
 }
 
