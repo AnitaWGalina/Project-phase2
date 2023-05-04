c340fa3444e2faf4d36b71bdbd7137a97f2a8800
paul kabuu <kongkabuu9@gmail.com>
2023-05-04 13:29:20 +0300

    created user

--- weightloss/src/App.js
+++ weightloss/src/App.js
@@ -1,13 +1,41 @@
-import About from "./components/About";
-import Faqs from "./components/Faqs";
+import { useState, useCallback, useEffect } from "react";
 
 function App() {
-  return (
-    <main className="App">
-      <About />
-      <Faqs />
-    </main>
-  );
+  // const [data, setData] = useState([]);
+  const url = "https://bespoke-diet-generator.p.rapidapi.com/user";
+  const options = {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+      "X-RapidAPI-Key": "01fa9544c8msh08b8a0d883a4ebdp1ff8a8jsne3e68d563a72",
+      "X-RapidAPI-Host": "bespoke-diet-generator.p.rapidapi.com",
+    },
+    body: JSON.stringify({
+      height: 184,
+      weight: 87,
+      dateOfBirth: "1991-03-03",
+      sex: "MALE",
+      activityLevel: "VERY_ACTIVE",
+    }),
+  };
+  const fetchUserData = useCallback(async () => {
+    try {
+      const response = await fetch(url, options);
+      if (!response.ok) {
+        throw new Error("error!");
+      }
+
+      const result = await response.json();
+      console.log(result);
+    } catch (error) {
+      console.error(error);
+    }
+  }, []);
+  useEffect(() => {
+    fetchUserData();
+  }, [fetchUserData]);
+
+  return <main className="App"></main>;
 }
 
 export default App;
---
+++ weightloss/src/components/Homepage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import DietGenerator from "./DietGenerator";
+
+function Homepage() {
+  return (
+    <div>
+      <DietGenerator />
+    </div>
+  );
+}
+
+export default Homepage;
